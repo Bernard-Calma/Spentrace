db9cbf47ef6c5b1ccb40cfed06afb0a38016cf8b
unknown <nard.calma@gmail.com>
2024-01-02 02:48:10 -0800

    feat(Month Changer): Create a month changer
    Create a view inside header that will handle as a month changer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,26 @@
 import { StyleSheet, Text, View } from "react-native"
 import Logo from "./Logo";
+import { useState } from "react";
+
+const months = ["January", "February", "March", "April"]
 
 const Header = () => {
+    const [date, setDate] = useState(new Date());
+    const [year, setYear] = useState(date.getFullYear())
     return(
         <View style={styles.header}>
             <Logo />
             <Text style={{
-                fontFamily: "roboto", 
+                fontFamily: "roboto-bold", 
                 fontSize: 18,
+                marginTop: 10
             }}>Budget</Text>
+            <View style={styles.monthChanger}>
+                <Text style={{
+                    fontFamily: "roboto", 
+                    fontSize: 16,
+                }}>{months[date.getMonth()]} {year}</Text>
+            </View>
         </View>
   
         
@@ -24,5 +36,8 @@ const styles = StyleSheet.create({
         alignItems: "center",
         // borderColor: "green", 
         // borderWidth: 1,
+    },
+    monthChanger: {
+        margin: 10
     }
 })
