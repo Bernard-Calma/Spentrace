084e6db7531da91cbcbd2bbc27e474034e82dfb3
unknown <nard.calma@gmail.com>
2024-01-11 00:40:06 -0800

    style(Title): Add style in title
    Add border and font style in title

--- src/views/BudgetList/components/Title.js
+++ src/views/BudgetList/components/Title.js
@@ -3,10 +3,10 @@ import { StyleSheet, Text, View } from "react-native";
 const Title = () => {
     return(
         <View style = {styles.titleContainer}>
-            <Text>Due Date</Text>
-            <Text>Account Name</Text>
-            <Text>Amount</Text>
-            <Text>Type</Text>
+            <Text style = {styles.titleText}>Due Date</Text>
+            <Text style = {styles.titleText}>Name</Text>
+            <Text style = {styles.titleText}>Amount</Text>
+            <Text style = {styles.titleText}>Type</Text>
         </View>
     )
 }
@@ -16,6 +16,15 @@ export default Title;
 const styles = StyleSheet.create({
     titleContainer: {
         flexDirection: "row",
-        justifyContent: "space-between"
+        justifyContent: "space-evenly",
+        borderWidth: 2,
+        borderColor: "black",
+    },
+    titleText: {
+        fontSize: 16,
+        textAlign: "center",
+        borderWidth: 1,
+        borderColor: "black",
+        flex: 1,
     }
 })
